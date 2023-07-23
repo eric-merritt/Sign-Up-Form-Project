7b16281cef0dc3c46c65a7ed55806089bfdc11a4
eric-merritt <ericm2009@gmail.com>
2023-07-23 17:08:56 -0600

    modified:   index.html 	modified:   script.js 	modified:   styles.css         -fixed issue letting mismatch passwords submit

--- script.js
+++ script.js
@@ -3,62 +3,117 @@
     const passConfirm = document.getElementById('password_confirm');
     const passwordDiv = document.querySelector('#password-cont');
     const submitBtn = document.querySelector('button.create-account');
+    const passClass = passInit.classList;
 
-    passConfirm.addEventListener('input', () => {
+    const inputs = document.querySelectorAll('input[required]');
 
-    if (passInit.value != passConfirm.value){
-        passInit.classList.add('mismatch');
-        passConfirm.classList.add('mismatch');
+    for (const input of inputs) {
 
-    } else {
-
-        passInit.classList.remove('mismatch');
-        passConfirm.classList.remove('mismatch');
-
-        const alertWarning = document.querySelector('.warn');
-        alertWarning.style.cssText = '';
-        alertWarning.textContent = '';
-        
+        input.addEventListener('blur',function(event){
+            event.target.classList.add('visited');
+        })
     }
-});
+    
+
+         
+    window.addEventListener('input', function(event){
+
+            if ((passInit.value != passConfirm.value) && !(passInit.classList.contains('mismatch'))){
+
+                passInit.classList.add('mismatch');
+
+            } else if (passInit.value == passConfirm.value){
+
+                passInit.classList.remove('mismatch');
+
+            }
+        
+        });
+
+        let alertWarning = document.querySelector('.warn');
+        let closeBtn = document.createElement('span');
+
+    
+    function makeWarn(){
+
+        alertWarning = document.querySelector('.warn');
+        alertWarning.style.cssText = 'background-color: red; padding: 10px; margin: 10px; opacity: 1; transition: opacity 0.6s';
+        alertWarning.textContent = 'Please correct the above issues in red'; 
+        alertWarning.style.display = 'none'; 
+
+        closeBtn.textContent = 'x';
+        closeBtn.className = 'closeBtn';
+        closeBtn.display = 'none';         
+        
+        closeBtn.addEventListener('click',()=>{
+             alertWarning.style.display = "none";
+         });
+         
+         alertWarning.appendChild(closeBtn);    
+         return alertWarning;
+    }
+
+window.onload = makeWarn();
+
+
+function toggleWarn(){
+
+
+if (alertWarning.style.display == 'none'){
+    alertWarning.style.display = 'block';
+    closeBtn.display = 'inline';
+} else {
+    alertWarning.style.display = 'none';
+    closeBtn.display = 'none';
+}
+}
+
+         
+
+        
+
+
+submitBtn.addEventListener('click', (event) => {
+
+    if (!inputs[0].checkValidity() || !inputs[1].checkValidity() || !inputs[2].checkValidity() || 
+        !inputs[3].checkValidity()||!(inputs[4].checkValidity()) || 
+        !inputs[5].checkValidity()) {
+
+            for (const input of inputs){ 
+
+
+                if (!input.classList.contains('visited')) {
+                    input.classList.add('visited');
+                }
+            }
+            
+            toggleWarn();
+            setTimeout(toggleWarn,5000);
+
+            event.preventDefault();
+ 
+        } else {
+
+            if (passInit.value == passConfirm.value){
+
+            return true;
+
+            } else {
+
+            toggleWarn();
+            setTimeout(toggleWarn,5000);
+            event.preventDefault();
+
+            }
+        }
+
+    });
+
+
 
     
 
-    submitBtn.addEventListener('click',(event) => {
-
-        const passClass = passInit.classList;
-
-        if (passClass.contains('mismatch')){
-
-    const alertWarning = document.querySelector('.warn');
-           alertWarning.style.cssText = 'background-color: red; padding: 10px; margin: 10px; opacity: 1; transition: opacity 0.6s';
-           alertWarning.textContent = 'Please correct the above issues in red'; 
-           event.preventDefault();
-
-            
                 
                 
 
-                const closeBtn = document.createElement('span');
-                closeBtn.textContent = 'x';
-                closeBtn.className = 'closeBtn';
-
-                closeBtn.addEventListener('click',()=>{
-                    alertWarning.style.display = "none";
-                });
-
-
-            alertWarning.appendChild(closeBtn);
-
-            
-
-
-                    
-
-        } else {
-
-            
-
-            dispatchEvent();
-        }
-    })
+                
